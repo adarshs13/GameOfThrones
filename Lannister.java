e56f5543bb38b66f818dbae7bfeb7a33599e4f6c
adarshs13 <adarshs13@users.noreply.github.com>
2014-11-09 17:38:51 -0500

    Create Lannister.java

---
+++ Lannister.java
@@ -0,0 +1,56 @@
+import java.awt.Rectangle;
+
+public class Lannister extends SouthHouse {
+
+    /**
+    * This is the Class constructor takes in two ints and a Rectangle. It then
+    * passes those values along with the House specific values to the super
+    * class constructor.
+    * @param xPos the x coordinate of the icon on the board
+    * @param yPos the y coordinate of the icon on the board
+    * @param bounds the rectangle that is the size of the board
+    */
+    public Lannister(int xPos, int yPos, Rectangle bounds) {
+        super("lion.png", 0, 25, xPos, yPos, 50, bounds);
+    }
+
+    @Override
+    public boolean canReproduceWithHouse(House otherHouse) {
+        int prob = (int) (Math.random() * 100) + 1;
+        if (this.collidesWithHouse(otherHouse)
+            && (otherHouse instanceof Lannister
+            || otherHouse instanceof Baratheon) && prob <= 17) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public House reproduceWithHouse(House otherHouse) {
+        if (this.canReproduceWithHouse(otherHouse)) {
+            return new Lannister(xPos, yPos, bounds);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean canHarmHouse(House otherHouse) {
+        int prob = (int) (Math.random() * 10) + 1;
+        if (this.collidesWithHouse(otherHouse)
+            && otherHouse instanceof Stark && prob <= 6) {
+            return true;
+        }
+        if (this.collidesWithHouse(otherHouse)
+            && otherHouse instanceof Tully && prob <= 8) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void harmHouse(House otherHouse) {
+        if (this.canHarmHouse(otherHouse)) {
+            otherHouse.health -= 4;
+        }
+    }
+}
