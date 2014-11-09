509d50cd58537d1f8767f9f1598dc315673780a3
adarshs13 <adarshs13@users.noreply.github.com>
2014-11-09 17:37:03 -0500

    Create NorthHouse.java

---
+++ NorthHouse.java
@@ -0,0 +1,50 @@
+import java.awt.Rectangle;
+
+public abstract class NorthHouse extends House {
+
+    protected static final int HEALTH_BONUS = 3;
+
+    /**
+    * This is the Class constructor it takes in a string, five ints,
+    * and one Rectangle. This constructor also checks if the click to provide
+    * the location or the House is out of bounds. If it is, the image is shifted
+    * up or to the left to bring it in bounds. It passes the values to the super
+    * class constructor.
+    * @param imageFilename the name of the file that will be displayed for the
+    * house
+    * @param age the staring age of the house
+    * @param maxAge the maximum age to which a house can live
+    * @param xPos the x coordinate of the icon on the board
+    * @param yPos the y coordinate of the icon on the board
+    * @param health the starting amount of health for the House
+    * @param bounds the rectangle that is the size of the board
+    */
+    public NorthHouse(String imageFilename, int age, int maxAge, int xPos,
+                int yPos, int health, Rectangle bounds) {
+        super(imageFilename, age, maxAge, xPos, yPos, health, bounds);
+        if (yPos > bounds.height - iconBound.height) {
+            yPos = bounds.height - iconBound.height - 1;
+            iconBound.setLocation(xPos, yPos);
+        }
+        if (xPos > bounds.width - iconBound.width) {
+            xPos = bounds.width - iconBound.width - 1;
+            iconBound.setLocation(xPos, yPos);
+        }
+    }
+
+    @Override
+    public void move() {
+        int x = (int) ((Math.random() * 40) - 20);
+        int y = (int) ((Math.random() * 40) - 20);
+        iconBound.translate(x, y);
+        if (!bounds.contains(iconBound)) {
+            iconBound.translate(-x, -y);
+        }
+        age++;
+        health--;
+        if (yPos <= 270) {
+            health += HEALTH_BONUS;
+        }
+    }
+
+}
